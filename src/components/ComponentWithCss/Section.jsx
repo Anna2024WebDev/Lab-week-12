427d2ce9ae749daf48e0de1ffc5b08b6ba1992f3
Anna <annah80@gmail.com>
2024-10-30 13:15:09 +0100

    first commit

---
+++ src/components/ComponentWithCss/Section.jsx
@@ -0,0 +1,12 @@
+// import { H2 } from "./ComponentWithCss/Headline.jsx"
+import "./Section.css"
+import { H2 } from "./Headline.jsx"
+
+export const Section = () => {
+  return (
+    <div className="section">
+      <H2 heading="title" />
+    </div>
+  )
+}
+
